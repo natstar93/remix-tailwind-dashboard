5d939e5f1b817ca031da8a348587baabd3efe8d0
Natalie Akam <natalie.akam@gmail.com>
2024-05-27 14:51:57 +0100

    Project setup

--- app/root.tsx
+++ app/root.tsx
@@ -4,14 +4,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
+import stylesheet from '~/tailwind.css?url';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' type='image/svg+xml' href='/accu-logo.svg' />
         <Meta />
         <Links />
       </head>
